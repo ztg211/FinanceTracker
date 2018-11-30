4c8b283ab129b759752fb18295394af11a00497c
Dean Kelly <dean.kelly@outlook.com>
2018-11-30 17:43:44 +0000

    Changed Date to day of month

--- _sql/D_LineItem.sql
+++ _sql/D_LineItem.sql
@@ -1,7 +1,7 @@
 CREATE TABLE D_LineItem ( 
 LI_Key INTEGER, 
 LI_Desc VARCHAR(100), 
-LI_InvoiceDate DATE,
+LI_PaymentDayofMonth INTEGER,
 LI_Frequency_Key INTEGER, 
 LI_PaymentType_Key INTEGER, 
 LI_SourceAccount_Key INTEGER, 
